cc46422eac37af738ae2a260dcfbf57e000671c4
Till Kahlbrock <till@jimdo.com>
2012-06-15 11:52:15 +0200

    BinarySearch ready

--- phpKatas/binarySearch/BinarySearch.php
+++ phpKatas/binarySearch/BinarySearch.php
@@ -2,7 +2,54 @@
 
 class BinarySearch
 {
-    public function chop($needle, $haystack) {
-        return -1;
+    public function chop($needle, $haystack)
+    {
+        $haystackSize = count($haystack);
+
+        if($haystackSize <= 0) {
+            return -1;
+        }
+
+        if ($haystackSize == 1 && $haystack[0] == $needle) {
+            return 0;
+        }
+
+        $from = 0;
+        $to = $haystackSize -1;
+
+        return $this->getPosition($needle, $haystack, $from, $to);
+    }
+
+    /**
+     * @param $needle
+     * @param $haystack
+     * @param $from
+     * @param $to
+     * @return int
+     */
+    private function getPosition($needle, $haystack, $from, $to)
+    {
+        $median = $this->getMedian($to, $from);
+
+        if ($haystack[$median] == $needle) {
+            return $median;
+        }
+
+        if ($haystack[$median] > $needle) {
+            $to = $median -1;
+        }
+
+        if ($haystack[$median] < $needle) {
+            $from = $median + 1;
+        }
+        return $this->getPosition($needle, $haystack, $from, $to);
+    }
+
+    private function getMedian($to, $from)
+    {
+        if ($to + $from <= 0) {
+            return 0;
+        }
+        return round(((($to + $from) - 1) / 2));
     }
 }
--- phpKatas/binarySearch/BinarySearchTest.php
+++ phpKatas/binarySearch/BinarySearchTest.php
@@ -16,4 +16,56 @@ class BinarySearchTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals(-1, $position);
     }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnThePostionOfANeedleThatsTheOnlyElementInAHaystack()
+    {
+        $haystack = array(1);
+        $needle = 1;
+        $search = new BinarySearch();
+        $position = $search->chop($needle, $haystack);
+
+        $this->assertEquals(0, $position);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnThePositionOfANeedleThatIsInTheLeftHalfOfTheHaystack()
+    {
+        $haystack = array(1,2,3);
+        $needle = 1;
+        $search = new BinarySearch();
+        $position = $search->chop($needle, $haystack);
+
+        $this->assertEquals(0, $position);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnThePositionOfANeedleThatIsInTheRightHalfOfTheHaystack()
+    {
+        $haystack = array(1,2,3);
+        $needle = 3;
+        $search = new BinarySearch();
+        $position = $search->chop($needle, $haystack);
+
+        $this->assertEquals(2, $position);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldReturnThePositionOfANeedleThatIsInTheMiddleOfTheHaystack()
+    {
+        $haystack = array(1,2,3);
+        $needle = 2;
+        $search = new BinarySearch();
+        $position = $search->chop($needle, $haystack);
+
+        $this->assertEquals(1, $position);
+    }
 }
